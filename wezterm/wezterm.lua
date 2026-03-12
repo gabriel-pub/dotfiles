eed1f689f3e6a926c80d8fe482d7478fd5ef2822
Gabriel Choong <gabrielchoonggeliang@gmail.com>
2026-03-12 14:51:13 +0800

    perf(opacity,blur): Remove unnecessary opacity and blur values
    Makes text harder to read with the previous values

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -16,8 +16,8 @@ config.color_scheme = "GruvboxDark"
 
 -- Other appearances
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.3
-config.macos_window_background_blur = 20
+-- config.window_background_opacity = 0.3
+-- config.macos_window_background_blur = 20
 config.window_decorations = "RESIZE"
 
 -- Fonts
